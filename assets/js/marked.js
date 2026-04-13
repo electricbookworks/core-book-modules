cb87382b5ba999c9c9e9861128d454153767d08b
Alexander Maughan <alex@maughan.me>
2026-04-13 12:53:52 +0200

    Fix conditional marked.js import

--- assets/js/marked.js
+++ assets/js/marked.js
@@ -1,7 +1,7 @@
-// Conditionally import marked only when not building for PDF
-// because marked is not compatible with PrinceXML.
+// Conditionally import marked only for web and app outputs.
+// Marked is not compatible with PrinceXML and EPUBs.
 let marked = null
-if (process.env.output !== 'print-pdf' && process.env.output !== 'screen-pdf') {
+if (process.env.output === 'web' || process.env.output === 'app') {
   marked = (await import('marked')).marked
 }
 export default marked
