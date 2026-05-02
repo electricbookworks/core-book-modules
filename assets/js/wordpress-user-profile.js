5bf0485e90e95e8942307dcc2580a5569f6efd70
Alexander Maughan <alex@maughan.me>
2026-05-02 09:31:57 +0200

    Add wordpress user profile js

---
+++ assets/js/wordpress-user-profile.js
@@ -0,0 +1,44 @@
+import { ebReplaceCanonicalURL } from '@electricbookworks/electric-book-modules/assets/js/utilities'
+
+const userDetailUrl = '/api/session/'
+const params = {
+  headers: {
+    'content-type': 'application/json; charset=UTF-8'
+  }
+}
+
+async function ebGetWPUserSession () {
+  try {
+    const userResponse = await fetch(userDetailUrl, params)
+    return userResponse.status === 200 ? await userResponse.json() : null
+  } catch (error) {
+    console.error('Error fetching user details:', error)
+  }
+}
+
+export default async function ebWordPressUserProfile () {
+  const userSession = await ebGetWPUserSession()
+
+  const profile = document.querySelector('.menu-user-profile')
+  const masthead = profile.closest('.masthead')
+
+  if (userSession) {
+    const avatar = profile.querySelector('.avatar')
+
+    avatar.classList.remove('visuallyhidden')
+    profile.querySelector('.buttons').classList.add('visuallyhidden')
+    profile.classList.remove('visuallyhidden')
+
+    // When we're logged in, let the masthead know for CSS reasons
+    masthead.classList.add('logged-in')
+  } else {
+    // First check whether we need to change the URL of the login button if we
+    // are not on the live server
+    const loginButton = profile.querySelector('.login')
+    ebReplaceCanonicalURL(loginButton)
+
+    // Then show the login and register buttons
+    profile.classList.remove('visuallyhidden')
+    profile.querySelector('.buttons').classList.remove('visuallyhidden')
+  }
+}
